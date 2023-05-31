f46534a464bbd1a48b843f6207fd39844904cc8e
Anthony Alabado <anthonyalabado3712@gmail.com>
2023-05-31 19:36:58 +0800

    First host web

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>For Hosting Practices only</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>Click the button to make numbers increment</p>
+      </div>
+      <p className="read-the-docs">Septriunii First Host Website</p>
+    </>
+  );
+}
+
+export default App;
